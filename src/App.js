39f2581b89a169d02b24b0fc16a10cc8dc5ecc1c
massimo <max@max-PC.(none)>
2021-03-26 13:04:08 +0100

    deploy react app to github pages

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import  React from 'react';
+import profileImage from './img/t.jpg';
+import './App.css'; 
+
+function App() {
+  return (
+    <div className="App">
+      <h1>welcome</h1>
+      <img src={profileImage}></img>
+    </div>
+  );
+}
+
+export default App;
